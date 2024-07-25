4b8184a1f4feb5b9bd0a60ecff585b725b925b3a
Nilton de Moura <153101984+NiltonXVII@users.noreply.github.com>
2024-07-25 11:55:57 -0300

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gêrencia Divar</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/logobb.ico">
+</head>
+
+<body class="black">   
+    <section class="quadrado">        
+        <div id="div_principal">
+            <img src="img/logo.bb2.png" class="img_divar">
+
+            <h1 class="index_titulo">Divar<h1>
+            <h2 class="index_subtitulo">Gerência Business Intelligence </h2>
+
+            <hr class="index_hr">
+
+            <div class="links">
+                <a class="botao" href="https://www.bb.com.br/site/promocoes/meu-credito-premiado/"target=-blank_>
+                    <img src="img/Svg.svg" class="index_svg">
+                    <span>Site BB</span> 
+                </a>
+                <a class="botao" href="https://divar.intranet.bb.com.br/dialoga/index.php"target=-blank_> 
+                    <img src="img/Svg.svg" class= "index_svg">
+                    <span>Dialoga 2.0</span>
+                </a>
+            </div>   
+
+            <section id="section_1">
+                <div>
+                    <div class="index_card">
+                        <img src="img/banco.png" class="img_banco">
+                    </div>
+                    <div class="index_content">
+                        <span class="index_titulo_site">Site BB</span>
+                        <p class="index_texto_site"> Contrate empréstimos pessoais no Banco do Brasil, encontre as melhores condições e ainda concorra a prêmios todo mês, serão 80 prêmios de 50.000 pontos de Livelo e um grande sorteio final de 1 milhão de pontos.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="index_card">
+                        <img src="img/banco2.png" class="img_banco">
+                    </div>
+                    <div class="index_content">
+                        <span class="index_titulo_dialoga">Dialoga 2.0</span>
+                        <p class="index_texto"> Gráficos-Dispersão dos funcionários avaliados nos eixos competência e placar de conexão. Matriz de Competências-Matriz com as nove competências da GDP, com ranking interno por competência. Extrato do funcionário.</p>
+                    </div>
+                </div>
+            </section> 
+
+        </div>
+    </section>
+
+    <div class="footer">
+        <div class="index_footer">
+            <img class="divar" src="img/LogoBI.png">
+            <span class="index_divar"> Gerência Business Intelligence</span> 
+        </div>
+    </div>
+
+</body>
+
+</html>
+
+
+
+    
+
+
+
+
+
+
+
+
